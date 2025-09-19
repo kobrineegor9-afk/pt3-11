0692290a307f19afa1df22c250ad01dd803399d8
student <kobrineegor9@gmail.com>
2025-09-19 19:36:56 +0300

    ggggggg

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-print('hello world!')
+import random
+
+def show_history(attempts_history):
+    print('/n history:')
+    for i, (guess, result) in enumerate(attempts_history, 1):
+        print(f'{i}, {guess} -> {result}')
+
+def main():
+    print('добро пожалаловать в игру кгадай число!')
+    print('я загадал число от 1 до 100,попробуй угадать!')
+
+    secret_number = random.randint(1,100)
+    attempts = 0
+    attempts_history = []
+
+    while True:
+        guess = input('your opinion').lower()
+        attempts += 1
+
+        if not guess.isdigit():
+            print("пожалуйсто введите число!")
+            continue
+
+        guess = int(guess)
+
+        if guess < secret_number:
+            print('загадочное число больше!')
+        elif guess > secret_number:
+            print('загаданное число меньше')
+        else:
+            print(f'ура ты угадал за {attempts} попыток')
+
+if __name__ == '__main__':
+    main()
+
